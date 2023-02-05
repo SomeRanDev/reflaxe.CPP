8b68b59e8e716c63e1667a2eca4f394f513792d0
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-02-05 06:22:45 -0500

    Submission script

---
+++ Submit.hx
@@ -0,0 +1,52 @@
+//====================================================================
+// * Haxe to GDScript Submit Script
+//
+// Haxelib is dumb and only allows for one class path, so this
+// script merges all the class paths (`src`, `std`, `std/gdscript/_std`)
+// into a single folder to help with haxelib submission.
+//====================================================================
+
+package;
+
+import sys.io.File;
+import haxe.io.Path;
+
+class Submit {
+	public static function main() {
+		if(sys.FileSystem.exists("_submit")) throw "'Folder /_submit/ should not exist before running this script.'";
+		makeDirIfNonExist("_submit");
+
+		copyDirContent("src", "_submit");
+		copyDirContent("std", "_submit", "std/gcf/_std");
+		copyDirContent("std/gcf/_std", "_submit", "", ".cross.hx");
+	}
+
+	static function makeDirIfNonExist(p: String) {
+		if(!sys.FileSystem.exists(p)) {
+			sys.FileSystem.createDirectory(p);
+		}
+	}
+	
+	static function copyDirContent(from: String, to: String, ignore: String = "", replaceExt: Null<String> = null) {
+		if(sys.FileSystem.exists(from)) {
+			for(file in sys.FileSystem.readDirectory(from)) {
+				final path = Path.join([from, file]);
+				var dest = Path.join([to, file]);
+				if(!sys.FileSystem.isDirectory(path)) {
+					if(replaceExt != null) {
+						dest = Path.withoutExtension(dest) + replaceExt;
+					}
+					File.copy(path, dest);
+				} else {
+					if(ignore.length > 0 && ignore == path) {
+						return;
+					}
+					final d = Path.addTrailingSlash(path);
+					final d2 = Path.addTrailingSlash(dest);
+					makeDirIfNonExist(d2);
+					copyDirContent(d, d2, ignore, replaceExt);
+				}
+			}
+		}
+	}
+}
