8d5e1f7911c018211a9a4a04487535968472af92
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-02-07 13:59:57 -0500

    Fix include for haxe.Log

--- std/gcf/_std/haxe/Log.hx
+++ std/gcf/_std/haxe/Log.hx
@@ -1,15 +1,15 @@
 package haxe;
 
+@:headerInclude("iostream", true)
 class Log {
-	public static function formatOutput(str: String, infos: PosInfos): String {
+	public static function formatOutput(str: String, infos: Null<PosInfos> = null): String {
 		if (infos == null)
 			return str;
 		var pstr = infos.fileName + ":" + infos.lineNumber;
 		return pstr + ": " + str;
 	}
 
-	@:include("iostream", true)
-	public static dynamic function trace(v: String, ?infos: PosInfos): Void {
+	public static dynamic function trace(v: String, infos: Null<PosInfos> = null): Void {
 		var str = formatOutput(v, infos);
 		untyped __cpp__("std::cout << {} << std::endl", str);
 	}
