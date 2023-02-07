2eb7540a89f5a87af694208bf6268a1ea9c6064d
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-02-07 06:01:42 -0500

    Implement enums

--- src/gcfcompiler/GCFCompiler.hx
+++ src/gcfcompiler/GCFCompiler.hx
@@ -436,12 +436,7 @@ class GCFCompiler extends reflaxe.BaseCompiler {
 			appendToExtraFile(headerFilename, compileIncludes(headerIncludes), 1);
 
 			var result = "";
-
-			for(p in classType.pack) {
-				result += "namespace " + p + " {\n";
-			}
-			if(classType.pack.length > 0) result += "\n";
-
+			result += compileNamespaceStart(classType);
 			result += header;
 
 			if(variables.length > 0) {
@@ -454,10 +449,7 @@ class GCFCompiler extends reflaxe.BaseCompiler {
 
 			result += "};\n";
 
-			if(classType.pack.length > 0) result += "\n";
-			for(p in classType.pack) {
-				result += "}\n";
-			}
+			result += compileNamespaceEnd(classType);
 
 			appendToExtraFile(headerFilename, result + "\n", 2);
 		}
@@ -465,6 +457,24 @@ class GCFCompiler extends reflaxe.BaseCompiler {
 		// We generated the files ourselves with "appendToExtraFile",
 		// so we return null so Reflaxe doesn't generate anything itself.
 		return null;
+	}
+
+	function compileNamespaceStart(md: CommonModuleTypeData): String {
+		var result = "";
+		for(p in md.pack) {
+			result += "namespace " + p + " {\n";
+		}
+		if(md.pack.length > 0) result += "\n";
+		return result;
+	}
+
+	function compileNamespaceEnd(md: CommonModuleTypeData): String {
+		var result = "";
+		if(md.pack.length > 0) result += "\n";
+		for(p in md.pack) {
+			result += "}\n";
+		}
+		return result;
 	}
 
 	function compileFunctionArgument(arg: { v: TVar, value: Null<TypedExpr> }, pos: Position) {
@@ -477,9 +487,147 @@ class GCFCompiler extends reflaxe.BaseCompiler {
 
 	// ----------------------------
 	// Compiles an enum into C++.
-	public function compileEnumImpl(enumType: EnumType, constructs: Map<String, haxe.macro.EnumField>): Null<String> {
+	public function compileEnumImpl(enumType: EnumType, options: EnumOptions): Null<String> {
+		// --------------------
+		// Init includes (always headers = true)
+		resetAndInitIncludes(true);
+
+		// --------------------
+		// Generate entire class declaration in "declaration"
+		final enumName = compileEnumName(enumType, enumType.pos, null, false);
+		var declaration = "class " + enumName + " {\npublic:\n";
+		declaration += "\tint index;\n\n";
+
+		var constructors = [];
+		var destructInfo = [];
+		var unionStructs = [];
+		var unionContent = [];
+
+		// --------------------
+		// Iterate through options
+		var index = 0;
+		for(o in options) {
+			final hasArgs = o.args.length > 0;
+			final args = o.args.map(opt -> [compileType(opt.t, o.field.pos), opt.name]);
+
+			// Generate static function "constructors"
+			{
+				final funcArgs = [];
+				for(i in 0...args.length) {
+					funcArgs.push(args[i][0] + " _" + (args.length == 1 ? o.name : args[i][1]) + (o.args[i].opt ? " = std::nullopt" : ""));
+				}
+				var construct = "";
+				construct += "static " + enumName + " " + o.name + "(" + funcArgs.join(", ") + ") {\n";
+				construct += "\t" + enumName + " result;\n";
+				construct += "\tresult.index = " + index + ";\n";
+				if(hasArgs) {
+					construct += "\tresult.data." + o.name + " = { " + args.map((tn) -> "_" + tn[1]).join(", ") + " };\n";
+				}
+				construct += "\treturn result;\n";
+				construct += "}";
+				constructors.push(construct);
+			}
+
+			// Generate private structs
+			if(hasArgs) {
+				var content = "";
+				final structName = "d" + o.name + "Impl";
+				content += "struct " + structName + " {\n";
+				content += args.map((tn) -> "\t" + tn.join(" ") + ";").join("\n");
+				content += "\n};";
+				unionStructs.push(content.tab());
+				destructInfo.push({ index: index, name: o.name, sname: structName });
+				unionContent.push("\t" + structName + " " + o.name + ";");
+			}
+
+			index++;
+		}
+
+		// --------------------
+		// Complete declaration
+
+		// Only create union if we need to
+		final requiresUnion = unionStructs.length > 0;
+		if(requiresUnion) {
+			declaration += unionStructs.join("\n\n") + "\n\n";
+
+			final unionIntro = "union dData {\n\tdData() {}\n\t~dData() {}\n";
+			final unionEnd = "\n};";
+			declaration += (unionIntro + unionContent.join("\n") + unionEnd).tab() + "\n\n";
+
+			declaration += "\tdData data;\n\n";
+		}
+		
+		// Create default constructor
+		declaration += (enumName + "() {\n\tindex = -1;\n}").tab() + "\n\n";
+
+		// If we're using a union, we need a copy constructor and destructor
+		if(requiresUnion) {
+			// --------------------
+			// Copy Constructor
+			var copyCon = enumName + "(const " + enumName + "& other) {\n\tindex = other.index;\n";
+
+			// Copy Constructor Body
+			{
+				var constructSwitch = "#define ConstructCase(index, cls) case index: { data.cls = other.data.cls; break; }\n";
+				for(d in destructInfo) {
+					constructSwitch += "ConstructCase(" + d.index + ", " + d.name + ")\n";
+				}
+				constructSwitch += "#undef ConstructCase";
+
+				copyCon += "\tswitch(index) {\n";
+				copyCon += constructSwitch.tab().tab();
+				copyCon += "\n\t}\n";
+			}
+
+			copyCon += "}";
+
+			// --------------------
+			// Destructor
+			var destruct = "~" + enumName + "() {\n";
+
+			// Destructor body
+			{
+				var destructSwitch = "#define DestructCase(index, field, cls) case index: { data.field.~cls(); break; }\n";
+				for(d in destructInfo) {
+					destructSwitch += "DestructCase(" + d.index + ", " + d.name + ", " + d.sname + ")\n";
+				}
+				destructSwitch += "#undef DestructCase";
+
+				destruct += "\tswitch(index) {\n";
+				destruct += destructSwitch.tab().tab();
+				destruct += "\n\t}\n";
+			}
+
+			destruct += "}";
+
+			// --------------------
+			// Add to declaration
+			declaration += copyCon.tab() + "\n\n";
+			declaration += destruct.tab() + "\n\n";
+		}
+
+		// Static function "constructors"
+		declaration += constructors.join("\n\n").tab();
+		declaration += "\n};";
+
+		// Start output
 		final filename = getFileNameFromModuleData(enumType);
-		setExtraFile("include/" + filename + headerExt, "#pragma once\n\n");
+		final headerFilename = "include/" + filename + headerExt;
+
+		// pragma once
+		setExtraFileIfEmpty(headerFilename, "#pragma once");
+
+		// Compile headers
+		appendToExtraFile(headerFilename, compileIncludes(headerIncludes), 1);
+
+		// Output class
+		var content = "";
+		content += compileNamespaceStart(enumType);
+		content += declaration;
+		content += compileNamespaceEnd(enumType);
+
+		appendToExtraFile(headerFilename, content, 2);
 
 		return null;
 	}
@@ -503,7 +651,10 @@ class GCFCompiler extends reflaxe.BaseCompiler {
 
 		final filename = getFileNameFromModuleData(defType);
 		final content = "typedef " + compileType(defType.type, defType.pos) + " " + defType.getNameOrNative() + ";";
-		setExtraFile("include/" + filename + headerExt, "#pragma once\n\n" + content);
+		final headerFilename = "include/" + filename + headerExt;
+
+		setExtraFileIfEmpty(headerFilename, "#pragma once");
+		appendToExtraFile(headerFilename, content, 2);
 
 		return null;
 	}
@@ -714,14 +865,14 @@ class GCFCompiler extends reflaxe.BaseCompiler {
 				switch(enumField.type) {
 					case TFun(args, _): {
 						if(index < args.length) {
-							result += "." + args[index].name;
+							result += (args.length == 1 ? ("." + enumField.name) : "") + "." + args[index].name;
 						}
 					}
 					case _:
 				}
 			}
 			case TEnumIndex(expr): {
-				result = compileExpression(expr) + "._index";
+				result = compileExpression(expr) + ".index";
 			}
 		}
 		return result;
