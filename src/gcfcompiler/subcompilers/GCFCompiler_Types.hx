abec1d66f413282d14a3eae40e0a1ecd0997be3e
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-02-09 11:24:10 -0500

    Account for explicit memory management classes in type compiling

--- src/gcfcompiler/subcompilers/GCFCompiler_Types.hx
+++ src/gcfcompiler/subcompilers/GCFCompiler_Types.hx
@@ -25,63 +25,6 @@ using gcfcompiler.helpers.GCFMeta;
 @:access(gcfcompiler.subcompilers.GCFCompiler_Includes)
 class GCFCompiler_Types extends GCFSubCompiler {
 	// ----------------------------
-	// Compile internal field of all ModuleTypes.
-	function compileModuleTypeName(typeData: { > NameAndMeta, pack: Array<String> }, pos: Position, params: Null<Array<Type>> = null, useNamespaces: Bool = true, asValue: Bool = false): String {
-		return if(typeData.hasNativeMeta()) {
-			var result = typeData.getNameOrNative();
-			result = StringTools.replace(result, "{this}", typeData.name);
-			if(params != null && params.length > 0) {
-				for(i in 0...params.length) {
-					result = StringTools.replace(result, "{arg" + i + "}", compileType(params[i], pos));
-				}
-			}
-			result;
-		} else {
-			final prefix = (useNamespaces ? typeData.pack.join("::") + (typeData.pack.length > 0 ? "::" : "") : "");
-			final innerClass = compileTypeNameWithParams(prefix + typeData.getNameOrNativeName(), pos, params);
-			final mmType = typeData.getMemoryManagementType();
-			if(asValue || mmType == Value) {
-				innerClass;
-			} else {
-				if(mmType == UnsafePtr) {
-					innerClass + "*";
-				} else if(mmType == UniquePtr) {
-					"std::unique_ptr<" + innerClass + ">";
-				} else {
-					"std::shared_ptr<" + innerClass + ">";
-				}
-			}
-		}
-	}
-
-	// ----------------------------
-	// Compile ClassType.
-	function compileClassName(classType: ClassType, pos: Position, params: Null<Array<Type>> = null, useNamespaces: Bool = true, asValue: Bool = false): String {
-		return compileModuleTypeName(classType, pos, params, useNamespaces, asValue);
-	}
-
-	// ----------------------------
-	// Compile EnumType.
-	function compileEnumName(enumType: EnumType, pos: Position, params: Null<Array<Type>> = null, useNamespaces: Bool = true, asValue: Bool = false): String {
-		return compileModuleTypeName(enumType, pos, params, useNamespaces, asValue);
-	}
-
-	// ----------------------------
-	// Compile DefType.
-	function compileDefName(defType: DefType, pos: Position, params: Null<Array<Type>> = null, useNamespaces: Bool = true, asValue: Bool = false): String {
-		return compileModuleTypeName(defType, pos, params, useNamespaces, asValue);
-	}
-
-	// ----------------------------
-	// Compiles type params.
-	function compileTypeNameWithParams(name: String, pos: Position, params: Null<Array<Type>> = null): Null<String> {
-		if(params == null || params.length == 0) {
-			return name;
-		}
-		return name + "<" + params.map(p -> compileType(p, pos)).join(", ") + ">";
-	}
-
-	// ----------------------------
 	// Compiles the provided type.
 	// Position must be provided for error reporting.
 	function compileType(t: Null<Type>, pos: Position, asValue: Bool = false): String {
@@ -152,9 +95,8 @@ class GCFCompiler_Types extends GCFSubCompiler {
 				if(prim != null) {
 					prim;
 				} else {
-
-					if(abs.isOverrideMemoryManagement()) {
-
+					if(abs.isOverrideMemoryManagement() && params.length == 1) {
+						return applyMemoryManagementWrapper(compileType(params[0], pos), abs.getMemoryManagementType());
 					}
 
 					switch(abs.name) {
@@ -163,7 +105,8 @@ class GCFCompiler_Types extends GCFSubCompiler {
 							"std::optional<" + compileType(params[0], pos) + ">";
 						}
 						case _: {
-							compileType(abs.type, pos);
+							final t = haxe.macro.TypeTools.applyTypeParameters(abs.type, abs.params, params);
+							compileType(t, pos);
 						}
 					}
 				}
@@ -171,6 +114,65 @@ class GCFCompiler_Types extends GCFSubCompiler {
 			case TType(defRef, params): {
 				compileDefName(defRef.get(), pos, params, true, asValue);
 			}
+		}
+	}
+
+	// ----------------------------
+	// Compile internal field of all ModuleTypes.
+	function compileModuleTypeName(typeData: { > NameAndMeta, pack: Array<String> }, pos: Position, params: Null<Array<Type>> = null, useNamespaces: Bool = true, asValue: Bool = false): String {
+		return if(typeData.hasNativeMeta()) {
+			var result = typeData.getNameOrNative();
+			result = StringTools.replace(result, "{this}", typeData.name);
+			if(params != null && params.length > 0) {
+				for(i in 0...params.length) {
+					result = StringTools.replace(result, "{arg" + i + "}", compileType(params[i], pos));
+				}
+			}
+			result;
+		} else {
+			final prefix = (useNamespaces ? typeData.pack.join("::") + (typeData.pack.length > 0 ? "::" : "") : "");
+			final innerClass = compileTypeNameWithParams(prefix + typeData.getNameOrNativeName(), pos, params);
+			final mmType = typeData.getMemoryManagementType();
+			applyMemoryManagementWrapper(innerClass, asValue ? Value : mmType);
+		}
+	}
+
+	// ----------------------------
+	// Compiles type params.
+	function compileTypeNameWithParams(name: String, pos: Position, params: Null<Array<Type>> = null): Null<String> {
+		if(params == null || params.length == 0) {
+			return name;
+		}
+		return name + "<" + params.map(p -> compileType(p, pos)).join(", ") + ">";
+	}
+
+	// ----------------------------
+	// Compile ClassType.
+	function compileClassName(classType: ClassType, pos: Position, params: Null<Array<Type>> = null, useNamespaces: Bool = true, asValue: Bool = false): String {
+		return compileModuleTypeName(classType, pos, params, useNamespaces, asValue);
+	}
+
+	// ----------------------------
+	// Compile EnumType.
+	function compileEnumName(enumType: EnumType, pos: Position, params: Null<Array<Type>> = null, useNamespaces: Bool = true, asValue: Bool = false): String {
+		return compileModuleTypeName(enumType, pos, params, useNamespaces, asValue);
+	}
+
+	// ----------------------------
+	// Compile DefType.
+	function compileDefName(defType: DefType, pos: Position, params: Null<Array<Type>> = null, useNamespaces: Bool = true, asValue: Bool = false): String {
+		return compileModuleTypeName(defType, pos, params, useNamespaces, asValue);
+	}
+
+	// ----------------------------
+	// Wrap compiled type based on the
+	// provided memory management type.
+	function applyMemoryManagementWrapper(inner: String, mmType: MemoryManagementType): String {
+		return switch(mmType) {
+			case Value: inner;
+			case UnsafePtr: inner + "*";
+			case UniquePtr: "std::unique_ptr<" + inner + ">";
+			case SharedPtr: "std::shared_ptr<" + inner + ">";
 		}
 	}
 }
