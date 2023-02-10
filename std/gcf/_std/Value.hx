ffd5b34f27b150ba149b55f9d7691c6daf319c3c
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-02-10 07:04:59 -0500

    Added @:forward to Value mm override

--- std/gcf/_std/Value.hx
+++ std/gcf/_std/Value.hx
@@ -1,9 +1,7 @@
 package;
 
-@:arrowAccess
 @:overrideMemoryManagement
 @:valueType
+@:forward
 extern abstract Value<T>(T) from T to T {
-	@:nativeFunctionCode("std::make_shared({this})")
-	@:to public extern function toShared(): SharedPtr<T>;
 }
