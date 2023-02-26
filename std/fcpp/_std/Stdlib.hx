49ab0f16035b85b272b90edb9dfb9a230a9a64c6
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-02-26 00:46:09 -0500

    Added Stdlib.sizeof

--- std/fcpp/_std/Stdlib.hx
+++ std/fcpp/_std/Stdlib.hx
@@ -8,4 +8,7 @@ extern class Stdlib {
 	@:native("free")
 	@:include("stdlib.h", true)
 	public static extern function free(p: Ptr<Void>): Void;
+
+	@:native("sizeof")
+	public static extern function sizeof(a: Dynamic): Int;
 }
