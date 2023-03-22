dd56c8363f2625b0db1392017f0f8da301161c79
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-03-22 19:34:21 -0400

    Added ucpp.std.Map

---
+++ std/ucpp/std/Map.hx
@@ -0,0 +1,28 @@
+package ucpp.std;
+
+@:nativeName("std::map")
+@:noHaxeNamespaces
+@:include("map", true)
+@:valueType
+extern class Map<T, U> {
+	public function new();
+
+	public function begin(): MapIterator<T, U>;
+	public function end(): MapIterator<T, U>;
+
+	@:const @:noExcept public function empty(): Bool;
+	@:const @:noExcept public function size(): ucpp.SizeT;
+	@:nativeName("max_size") @:const @:noExcept public function maxSize(): ucpp.SizeT;
+	public function at(key: T): U;
+	public function insert(values: ucpp.std.Pair<T, U>): Void;
+	public function erase(key: T): ucpp.SizeT;
+	@:noExcept public function clear(): Void;
+	public function find(key: T): MapIterator<T, U>;
+	@:const public function count(key: T): ucpp.SizeT;
+}
+
+@:forward
+@:native("auto")
+@:noInclude
+extern abstract MapIterator<T, U>(ucpp.Ptr<ucpp.std.Pair<T, U>>) from ucpp.Ptr<ucpp.std.Pair<T, U>> to ucpp.Ptr<ucpp.std.Pair<T, U>> {
+}
