c442fdc370619cc9b91f0ce009a6b6d465d80c6a
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-03-22 19:38:06 -0400

    Added increment, decrement, and toValue for Ptr

--- std/ucpp/Ptr.hx
+++ std/ucpp/Ptr.hx
@@ -7,4 +7,13 @@ package ucpp;
 extern abstract Ptr<T>(T) from T to T {
 	@:nativeFunctionCode("{this} == nullptr")
 	public function isNull(): Bool;
+
+	@:nativeFunctionCode("({this}++)")
+	public function increment(): Void;
+
+	@:nativeFunctionCode("({this}--)")
+	public function decrement(): Void;
+
+	@:nativeFunctionCode("(*{this})")
+	public function toValue(): ucpp.Value<T>;
 }
