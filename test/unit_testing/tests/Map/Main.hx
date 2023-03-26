8f13299651b1b08066800c16411e3f024b619003
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-03-26 03:35:18 -0400

    Filled out Map test with IntMap testing

--- test/unit_testing/tests/Map/Main.hx
+++ test/unit_testing/tests/Map/Main.hx
@@ -1,6 +1,5 @@
 package;
 
-import ucpp.std.Pair;
 import haxe.PosInfos;
 
 class Main {
@@ -14,22 +13,61 @@ class Main {
 	}
 
 	public static function main() {
-		final bla = [1 => 123];
-		trace(bla[1]);
+		final intMap = [1 => 123];
 
-		final a = new ucpp.std.Map<Int, Int>();
+		final intMap2 = intMap.copy();
 
-		a.insert(new Pair(12, 24));
+		intMap.set(2, 222);
 
-		trace(a.at(12));
+		assert(intMap.get(1) == 123);
+		assert(intMap.get(2) == 222);
 
-		var it = a.begin();
+		assert(intMap.exists(1));
+		assert(intMap.exists(2));
+		assert(!intMap.exists(3));
 
-		trace(it.first);
+		intMap.set(3, 333);
+		assert(intMap.exists(3));
+		assert(intMap.remove(3));
+		assert(!intMap.remove(3));
 
-		it.increment();
+		var sum = 0;
+		for(k in intMap.keys()) {
+			sum += k;
+		}
+		assert(sum == 3);
 
-		trace(it == a.end());
+		final it = intMap.iterator();
+		sum = 0;
+		while(it.hasNext()) {
+			sum += it.next();
+		}
+		assert(sum == (123 + 222));
+
+		var index = 0;
+		for(test in intMap.keyValueIterator()) {
+			switch(index++) {
+				case 0: {
+					assert(test.key == 1);
+					assert(test.value == 123);
+				}
+				case 1: {
+					assert(test.key == 2);
+					assert(test.value == 222);
+				}
+				case _: assert(false);
+			}
+		}
+
+		assert(intMap.toString() == "[1 => 123, 2 => 222]");
+
+		assert(intMap2[1] == 123);
+		assert(intMap2.get(2) == null);
+
+		intMap2.clear();
+
+		assert(intMap2.toString() == "[]");
+		assert(intMap2[1] == null);
 
 		if(returnCode != 0) {
 			Sys.exit(returnCode);
