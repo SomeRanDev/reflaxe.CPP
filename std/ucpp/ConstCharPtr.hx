ac57c6a8408805dac528223f97956fb7b2e59eba
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-04-03 23:15:18 -0400

    Added ucpp.ConstCharPtr.toString

--- std/ucpp/ConstCharPtr.hx
+++ std/ucpp/ConstCharPtr.hx
@@ -1,5 +1,9 @@
 package ucpp;
 
 @:native("const char*")
+@:valueType
 extern class ConstCharPtr {
+	@:nativeFunctionCode("std::string({this})")
+	@:include("string", true)
+	public function toString(): String;
 }
