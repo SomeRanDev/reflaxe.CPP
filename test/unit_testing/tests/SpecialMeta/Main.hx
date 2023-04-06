7ba4eca530acad472fbc492d930581bb8838be8c
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-04-05 20:15:43 -0400

    Added SpecialMeta test

---
+++ test/unit_testing/tests/SpecialMeta/Main.hx
@@ -0,0 +1,24 @@
+package;
+
+@:headerCode("#define SOME_API_THING")
+@:classNamePrefix("SOME_API_THING")
+@:prependContent("// This class was generated from Haxe.\n")
+@:appendContent("\n// End of Main class")
+class Main {
+	@:prependContent("// Returns an int of value 123\n")
+	@:prependContent("[[nodiscard]] ")
+	@:appendContent("\n// Comment after the function")
+	public static function getValue(): Int {
+		return 123;
+	}
+
+	@:meta(nodiscard)
+	public static function getValue2(): String {
+		return "String";
+	}
+
+	public static function main() {
+		trace(getValue());
+		trace(getValue2());
+	}
+}
