0bd3f47bcf84fdfb1bec1ccf07a01a79a7800f55
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-04-08 16:58:02 -0400

    Array<T> (std::deque) support in DynamicToString

--- std/ucpp/DynamicToString.hx
+++ std/ucpp/DynamicToString.hx
@@ -8,6 +8,12 @@ struct has_to_string : std::false_type { };
 template<typename T>
 struct has_to_string <T, decltype(std::declval<T>().toString())> : std::true_type { };
 
+template<typename>
+struct is_deque : std::false_type {};
+
+template<typename T>
+struct is_deque<std::deque<T>> : std::true_type {};
+
 struct DynamicToString: public std::string {
     template<typename T>
     DynamicToString(T s): std::string(ToString(s)) {}
@@ -33,8 +39,14 @@ struct DynamicToString: public std::string {
 			return s.toString();
 		} else if constexpr(haxe::_unwrap_mm<T>::can_deref) {
 			return ToString(*s);
+		} else if constexpr(is_deque<T>::value) {
+			std::string result = \"[\";
+			for(int i = 0; i < s.size(); i++) {
+				result += (i > 0 ? \", \" : \"\") + ToString(s[i]);
+			}
+			return result + \"]\";
 		}
-		
+
 		// Print address if all else fails
 		std::stringstream pointer_stream;
 		pointer_stream << std::addressof(s);
