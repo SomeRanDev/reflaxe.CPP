9c9f9098f32f35f1037baf6afe1846136a8eb250
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-04-09 10:16:07 -0400

    Added std.chrono.Duration

---
+++ std/ucpp/std/chrono/Duration.hx
@@ -0,0 +1,17 @@
+package ucpp.std.chrono;
+
+@:nativeName("std::chrono::duration")
+@:valueType
+extern class Duration {
+	public function count(): Int;
+
+	public static function zero(): Duration;
+	public static function min(): Duration;
+	public static function max(): Duration;
+
+	@:nativeFunctionCode("std::chrono::duration_cast<std::chrono::seconds>({this})")
+	public function toSeconds(): Duration;
+
+	@:nativeFunctionCode("std::chrono::duration_cast<std::chrono::milliseconds>({this})")
+	public function toMilliseconds(): Duration;
+}
