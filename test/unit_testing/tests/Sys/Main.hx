7a1177a3f563200a3eba498cd4614cb9bbaeb8a0
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-04-09 10:19:14 -0400

    Added to Sys test (getCwd, time, cpuTime, sleep)

--- test/unit_testing/tests/Sys/Main.hx
+++ test/unit_testing/tests/Sys/Main.hx
@@ -33,6 +33,34 @@ class Main {
 		assert(args.length == 1);
 		assert(args[0].indexOf("test_out") >= 0);
 
+		// Sys.getCwd
+		// Not consistent enough to test, so we'll just print and see if it crashes.
+		trace(Sys.getCwd());
+
+		#if windows
+		Sys.setCwd("C:/");
+		assert(Sys.getCwd() == "C:\\");
+		#end
+
+		// Sys.time
+		trace(Sys.time());
+
+		// ---
+		// Check both "Sys.sleep" and "Sys.cpuTime"
+
+		// Get time before sleep.
+		final beforeSleep = Sys.cpuTime();
+
+		// Sys.sleep
+		Sys.sleep(1.3);
+
+		// Duration of sleep.
+		final sleepTime =  Sys.cpuTime() - beforeSleep;
+
+		// Check if "sleepTime" is around 1.3 seconds
+		assert(sleepTime > 1.29 && sleepTime < 1.31);
+		trace("sleepTime = " + sleepTime); // Debug purposes
+
 		if(returnCode != 0) {
 			Sys.exit(returnCode);
 		}
