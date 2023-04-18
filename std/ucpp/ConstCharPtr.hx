0fc660bebbe00cc2b7cddb86bbcc92c612fa6cb3
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-04-17 20:43:29 -0400

    Added ucpp.ConstCharPtr.fromString

--- std/ucpp/ConstCharPtr.hx
+++ std/ucpp/ConstCharPtr.hx
@@ -6,4 +6,8 @@ extern class ConstCharPtr {
 	@:nativeFunctionCode("std::string({this})")
 	@:include("string", true)
 	public function toString(): String;
+
+	public static extern inline function fromString(s: String): ConstCharPtr {
+		return @:privateAccess s.c_str();
+	}
 }
