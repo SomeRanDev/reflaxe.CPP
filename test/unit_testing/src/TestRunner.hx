3667e0ea60065a978c19a758bbf57f8e59eba87b
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-05-12 04:43:06 -0400

    Actually, don't add callstacks to tests

--- test/unit_testing/src/TestRunner.hx
+++ test/unit_testing/src/TestRunner.hx
@@ -205,7 +205,6 @@ function executeTests(testDir: String, hxmlFiles: Array<String>): Bool {
 			"-cp " + testDir,
 			"-D cpp-output=" + haxe.io.Path.join([testDir, getOutputDirectory(testDir)]),
 			"-D " + systemNameDefine,
-			"-D cxx_callstack",
 			"\"" + absPath + "\""
 		];
 
