fca2dee8d1cdd62261c1751dd22a7601d8032206
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-05-15 03:27:02 -0400

    Added std::cout/cerr/clog

---
+++ std/cxx/Std.hx
@@ -0,0 +1,18 @@
+package cxx;
+
+@:cxxStd
+extern class Std {
+	@:native("std::cout")
+	public static var cout: cxx.Value<cxx.std.ios.OStream>;
+
+	@:native("std::cerr")
+	public static var cerr: cxx.Value<cxx.std.ios.OStream>;
+
+	@:native("std::clog")
+	public static var clog: cxx.Value<cxx.std.ios.OStream>;
+
+	// TODO:
+	// public static var wcout: cxx.std.WOStream;
+	// public static var wcerr: cxx.std.WOStream;
+	// public static var wclog: cxx.std.WOStream;
+}
