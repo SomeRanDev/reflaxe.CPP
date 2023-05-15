0a51febf944b14dc5310ddc166b47f9c410c5ce7
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-05-15 11:07:53 -0400

    Add CArray.toPtr and ConstCharPtr.fromCharPtr

--- std/cxx/ConstCharPtr.hx
+++ std/cxx/ConstCharPtr.hx
@@ -11,4 +11,8 @@ extern class ConstCharPtr {
 	public static extern inline function fromString(s: String): ConstCharPtr {
 		return @:privateAccess s.c_str();
 	}
+
+	public static extern inline function fromCharPtr(p: Ptr<Char>): ConstCharPtr {
+		return cast p;
+	}
 }
