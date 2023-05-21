cc64a1469e656c08f324f4f9b9e2a9143265701d
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-05-21 13:32:10 -0400

    Add @:noReflaxeSanitize to cxx.Syntax

--- std/cxx/Syntax.hx
+++ std/cxx/Syntax.hx
@@ -2,6 +2,7 @@ package cxx;
 
 @:cxxStd
 @:noClosure
+@:noReflaxeSanitize
 extern class Syntax {
 	public static function classicFor(start: Dynamic, cond: Dynamic, inc: Dynamic, block: Dynamic): Void;
 }
