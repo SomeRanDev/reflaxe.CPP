c8f3ef202c0bea6cb4af7b9165d27c07da6126cf
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-06-20 00:39:34 -0400

    Added Syntax.toPointer

--- std/cxx/Syntax.hx
+++ std/cxx/Syntax.hx
@@ -5,4 +5,7 @@ package cxx;
 @:noReflaxeSanitize
 extern class Syntax {
 	public static function classicFor(start: Dynamic, cond: Dynamic, inc: Dynamic, block: Dynamic): Void;
+
+	@:nativeFunctionCode("&({arg0})")
+	public static function toPointer<T>(obj: T): cxx.Ptr<T>;
 }
