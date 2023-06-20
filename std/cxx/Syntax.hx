330c6951eb2a18d8fa7c65867b8374acb16f1366
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2023-06-20 02:04:56 -0400

    Document Syntax functions

--- std/cxx/Syntax.hx
+++ std/cxx/Syntax.hx
@@ -4,11 +4,27 @@ package cxx;
 @:noClosure
 @:noReflaxeSanitize
 extern class Syntax {
-	public static function classicFor(start: Dynamic, cond: Dynamic, inc: Dynamic, block: Dynamic): Void;
-
+	/**
+		Allows for arbitrary use of `&` prefix operator in C++.
+	**/
 	@:nativeFunctionCode("&({arg0})")
 	public static function toPointer<T>(obj: T): cxx.Ptr<T>;
 
+	/**
+		Allows for arbitrary use of `*` prefix operator in C++.
+	**/
 	@:nativeFunctionCode("*({arg0})")
 	public static function deref(obj: cxx.Untyped): cxx.Untyped;
+
+	/**
+		Generates a C for loop.
+
+		The arguments are filled in like this:
+		```cpp
+		for($start; $cond; $inc) {
+			$block;
+		}
+		```
+	**/
+	public static function classicFor(start: Dynamic, cond: Dynamic, inc: Dynamic, block: Dynamic): Void;
 }
