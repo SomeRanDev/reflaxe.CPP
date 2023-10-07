a0b63cb7e7df1b24d049b227bd2bf89fdf570355
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2023-10-07 06:01:06 -0400

    Add CMake tests

---
+++ test/unit_testing/tests/CMake/intended/src/Main.cpp
@@ -0,0 +1,10 @@
+#include "Main.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std::string_literals;
+
+void _Main::Main_Fields_::main() {
+	std::cout << "test/unit_testing/tests/CMake/Main.hx:4: Hello world!"s << std::endl;
+}
