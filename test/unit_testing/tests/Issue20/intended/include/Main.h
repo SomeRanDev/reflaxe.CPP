338492d9c6854470ed787a3643d2a5b7ee8d4c2a
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2023-10-12 15:19:12 -0400

    Update Issue20 test

--- test/unit_testing/tests/Issue20/intended/include/Main.h
+++ test/unit_testing/tests/Issue20/intended/include/Main.h
@@ -1,26 +1,10 @@
 #pragma once
 
-#include <optional>
-
 namespace _Main {
 
 class Main_Fields_ {
 public:
 	static void main();
-};
-
-}
-
-
-namespace _Main {
-
-class Flexible_Impl_ {
-public:
-	static int _new();
-	template<typename T>
-	static T toAny(int this1) {
-		return nullptr;
-	}
 };
 
 }
--- test/unit_testing/tests/Issue20/intended/src/Main.cpp
+++ test/unit_testing/tests/Issue20/intended/src/Main.cpp
@@ -1,19 +1,5 @@
 #include "Main.h"
 
-#include <memory>
-#include <string>
-#include "_AnonStructs.h"
-#include "haxe_Log.h"
-
-using namespace std::string_literals;
-
 void _Main::Main_Fields_::main() {
-	std::string r = _Main::Flexible_Impl_::toAny<std::string>(_Main::Flexible_Impl_::_new());
 
-	haxe::Log::trace(r, haxe::shared_anon<haxe::PosInfos>("_Main.Main_Fields_"s, "test/unit_testing/tests/Issue20/Main.hx"s, 13, "main"s));
-}
-int _Main::Flexible_Impl_::_new() {
-	int this1 = 1;
-
-	return this1;
 }
